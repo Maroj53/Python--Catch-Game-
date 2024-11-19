c6c84c4d8739d15e632bda0fbd56f1d1faedb69a
Martin Dillinger <Maroj.Dillinger@gmail.com>
2024-11-19 17:47:54 +0200

    Update CatchGame-PreDef-4-Debug-Commented.py

--- CatchGame-PreDef-4-Debug-Commented.py
+++ CatchGame-PreDef-4-Debug-Commented.py
@@ -29,8 +29,8 @@ Playerspawnlocationx = 1160                   #- Setting up variables
 Playerspawnlocationy = 860                  #- Setting up variables
 Zombiespawnlocationx = 10                   #- Setting up variables
 Zombiespawnlocationy = 10                   #- Setting up variables
-ClassPlayer = 0
-ClassZombie = 0
+ClassPlayer = 1
+ClassZombie = 1
 
 
 class Zombies:
@@ -54,15 +54,15 @@ class Zombies:
     
 
 class Player:
-  def __init__ (self, centerY, centerX, WalkSpeed, size,Health):
+  def __init__ (self, centerY, centerX, WalkSpeed, size1,Health):
     self.Health = Health   
     self.centerX = centerX   
     self.centerY = centerY
     self.WalkSpeed = WalkSpeed   
-    self.size = size   
-    self.Surface = pygame.Surface ([self.size,self.size], pygame.SRCALPHA, 32)    #- Game surface
+    self.size1 = size1   
+    self.Surface = pygame.Surface ([self.size1,self.size1], pygame.SRCALPHA, 32)    #- Game surface
     self.Surface.fill ([200,200,200])                    #- fill surface with a color
-    pygame.draw.circle (self.Surface, [255,0,0], [self.centerX, self.centerY], self.size)        #- draw a circle 
+    pygame.draw.circle (self.Surface, [255,0,0], [self.centerX, self.centerY], self.size1)        #- draw a circle 
     self.PlayerRect = self.Surface.get_rect (x=Playerspawnlocationx, y=Playerspawnlocationy)        #- set the position of the top-left corner
   def draw (self, Window):
     """ Drawing into the window
@@ -72,7 +72,7 @@ class Player:
     
 
 Player1 = Player(Playerspawnlocationx, Playerspawnlocationy, 4, 10,1000) #- Note: 'Player1' is now created from the 'Player' class
-Player2 = Player(Playerspawnlocationx, Playerspawnlocationy, 4, 10,700) #- Note: 'Player2' is now created from the 'Player' class
+Player2 = Player(Playerspawnlocationx, Playerspawnlocationy, 4, 20,700) #- Note: 'Player2' is now created from the 'Player' class
 
 Zombie1 = Zombies(Zombiespawnlocationx, Zombiespawnlocationy, 5, 20)  #- Note: 'Zombie1' is now created from the 'Zombies' class
 Zombie2 = Zombies(Zombiespawnlocationx, Zombiespawnlocationy, 5, 20)  #- Note: 'Zombie2' is now created from the 'Zombies' class
@@ -97,75 +97,84 @@ while MainLoop:
   
   #- Drawing
   Window.fill ([0,0,0])                    #- fill the window with black color
+  
 
   #- Program
-  SprintBarX = Stamina                          #- Setting up variables
-  
-  if Player1.PlayerRect.colliderect (Zombie1.ZombieRect2):
-      Player1.Health = Player1.Health - 10
-      
-  if Stamina >= 1000:
-      Stamina = 1000
-      
-      
-  if Zombiesclasses[ClassZombie].ZombieRect2.x >= WindowWidth-Zombiesclasses[ClassZombie].size:
-    Zombiesclasses[ClassZombie].ZombieRect2.x = WindowWidth-Zombiesclasses[ClassZombie].size
-  if Zombiesclasses[ClassZombie].ZombieRect2.x <= 0:
-    Zombiesclasses[ClassZombie].ZombieRect2.x = 0
-  if Zombiesclasses[ClassZombie].ZombieRect2.y >= WindowHeight-Zombiesclasses[ClassZombie].size:
-    Zombiesclasses[ClassZombie].ZombieRect2.y = WindowHeight-Zombiesclasses[ClassZombie].size
-  if Zombiesclasses[ClassZombie].ZombieRect2.y <= 0:
-    Zombiesclasses[ClassZombie].ZombieRect2.y = 0
-
-  if Playersclasses[ClassPlayer].PlayerRect.x >= WindowWidth-Playersclasses[ClassPlayer].size*2:
-    Playersclasses[ClassPlayer].PlayerRect.x = WindowWidth-Playersclasses[ClassPlayer].size*2
-  if Playersclasses[ClassPlayer].PlayerRect.x <= 0:
-    Playersclasses[ClassPlayer].PlayerRect.x = 0
-  if Playersclasses[ClassPlayer].PlayerRect.y >= WindowHeight-Playersclasses[ClassPlayer].size*2:
-    Playersclasses[ClassPlayer].PlayerRect.y = WindowHeight-Playersclasses[ClassPlayer].size*2
-  if Playersclasses[ClassPlayer].PlayerRect.y <= 0:
-    Playersclasses[ClassPlayer].PlayerRect.y = 0
-
-  Window.blit (Player1.Surface, Player1.PlayerRect)  #- draw the player on the screen
-  Window.blit (Zombie1.Surface2, Zombie1.ZombieRect2)  #- draw the zombie on the screen
-  
-  pygame.draw.rect (Window, [255,0,0], (100,WindowHeight-100,SprintBarX,10))
-  pygame.draw.rect (Window, [0,255,0], (100,WindowHeight-50,Player1.Health,10))
-  
-  #- Controls
-  KeyboardState = pygame.key.get_pressed()            #- get the state of the keyboard
-
-  if KeyboardState[pygame.K_RIGHT]:                #- if right arrow key is pressed
-    Player1.PlayerRect.x = Player1.PlayerRect.x + Player1.WalkSpeed        
-  if KeyboardState[pygame.K_LEFT]:                #- if left arrow key is pressed
-    Player1.PlayerRect.x = Player1.PlayerRect.x - Player1.WalkSpeed            
-  if KeyboardState[pygame.K_UP]:                #- if up arrow key is pressed
-    Player1.PlayerRect.y = Player1.PlayerRect.y - Player1.WalkSpeed        
-  if KeyboardState[pygame.K_DOWN]:                #- if down arrow key is pressed
-    Player1.PlayerRect.y = Player1.PlayerRect.y + Player1.WalkSpeed        
+  def Program(ZombieClasses1,Playersclasses1,SprintBarX):
     
-  if KeyboardState[pygame.K_d]:                #- if 'd' key is pressed
-    Zombie1.ZombieRect2.x = Zombie1.ZombieRect2.x + Zombie1.WalkSpeed        
-  if KeyboardState[pygame.K_a]:                #- if 'a' key is pressed
-    Zombie1.ZombieRect2.x = Zombie1.ZombieRect2.x - Zombie1.WalkSpeed        
-  if KeyboardState[pygame.K_w]:                #- if 'w' key is pressed
-    Zombie1.ZombieRect2.y = Zombie1.ZombieRect2.y - Zombie1.WalkSpeed        
-  if KeyboardState[pygame.K_s]:                #- if 's' key is pressed
-    Zombie1.ZombieRect2.y = Zombie1.ZombieRect2.y + Zombie1.WalkSpeed  
-
-  if KeyboardState[pygame.K_LSHIFT] and Stamina > 0:                #- if 'Left Shift' key is pressed
-    Zombie1.WalkSpeed   = 6
-    Stamina = Stamina - 10
-  else: 
-    Zombie1.WalkSpeed   = 3                         #- Sprint Ability
+    global Stamina
+    SprintBarX = Stamina                          #- Setting up variables
     
-    
-  pygame.display.flip()                    #- update the screen with the new drawing
 
-  #- Time delay
-  Clock.tick (Fps)                    #- delay the loop according to the set FPS (frames per second)
+
+    if Stamina >= 1000:
+        Stamina = 1000
+        
+    if ZombieClasses1.ZombieRect2.x >= WindowWidth-ZombieClasses1.size:
+        ZombieClasses1.ZombieRect2.x = WindowWidth-ZombieClasses1.size
+    if ZombieClasses1.ZombieRect2.x <= 0:
+        ZombieClasses1.ZombieRect2.x = 0
+    if ZombieClasses1.ZombieRect2.y >= WindowHeight-ZombieClasses1.size:
+        ZombieClasses1.ZombieRect2.y = WindowHeight-ZombieClasses1.size
+    if ZombieClasses1.ZombieRect2.y <= 0:
+        ZombieClasses1.ZombieRect2.y = 0
+
+    if Playersclasses1.PlayerRect.colliderect (Zombie1.ZombieRect2):
+        Playersclasses1.Health = Playersclasses1.Health - 10
+
+    if Playersclasses1.PlayerRect.x >= WindowWidth-Playersclasses1.size1*2:
+        Playersclasses1.PlayerRect.x = WindowWidth-Playersclasses1.size1*2
+    if Playersclasses1.PlayerRect.x <= 0:
+        Playersclasses1.PlayerRect.x = 0
+    if Playersclasses1.PlayerRect.y >= WindowHeight-Playersclasses1.size1*2:
+        Playersclasses1.PlayerRect.y = WindowHeight-Playersclasses1.size1*2
+    if Playersclasses1.PlayerRect.y <= 0:
+        Playersclasses1.PlayerRect.y = 0      
+
+
+    Window.blit (Playersclasses1.Surface, Playersclasses1.PlayerRect)  #- draw the player on the screen
+    Window.blit (Zombie1.Surface2, Zombie1.ZombieRect2)  #- draw the zombie on the screen
+    
+    pygame.draw.rect (Window, [255,0,0], (100,WindowHeight-100,SprintBarX,10))
+    pygame.draw.rect (Window, [0,255,0], (100,WindowHeight-50,Playersclasses1.Health,10))
+    
+    #- Controls
+    KeyboardState = pygame.key.get_pressed()            #- get the state of the keyboard
+
+    if KeyboardState[pygame.K_RIGHT]:                #- if right arrow key is pressed
+        Playersclasses1.PlayerRect.x = Playersclasses1.PlayerRect.x + Playersclasses1.WalkSpeed        
+    if KeyboardState[pygame.K_LEFT]:                #- if left arrow key is pressed
+        Playersclasses1.PlayerRect.x = Playersclasses1.PlayerRect.x - Playersclasses1.WalkSpeed            
+    if KeyboardState[pygame.K_UP]:                #- if up arrow key is pressed
+        Playersclasses1.PlayerRect.y = Playersclasses1.PlayerRect.y - Playersclasses1.WalkSpeed        
+    if KeyboardState[pygame.K_DOWN]:                #- if down arrow key is pressed
+        Playersclasses1.PlayerRect.y = Playersclasses1.PlayerRect.y + Playersclasses1.WalkSpeed        
+        
+    if KeyboardState[pygame.K_d]:                #- if 'd' key is pressed
+        ZombieClasses1.ZombieRect2.x = ZombieClasses1.ZombieRect2.x + ZombieClasses1.WalkSpeed        
+    if KeyboardState[pygame.K_a]:                #- if 'a' key is pressed
+        ZombieClasses1.ZombieRect2.x = ZombieClasses1.ZombieRect2.x - ZombieClasses1.WalkSpeed        
+    if KeyboardState[pygame.K_w]:                #- if 'w' key is pressed
+        ZombieClasses1.ZombieRect2.y = ZombieClasses1.ZombieRect2.y - ZombieClasses1.WalkSpeed        
+    if KeyboardState[pygame.K_s]:                #- if 's' key is pressed
+        ZombieClasses1.ZombieRect2.y = ZombieClasses1.ZombieRect2.y + ZombieClasses1.WalkSpeed  
+
+    if KeyboardState[pygame.K_LSHIFT] and Stamina > 0:                #- if 'Left Shift' key is pressed
+        ZombieClasses1.WalkSpeed   = 6
+        Stamina = Stamina - 10
+    else: 
+        ZombieClasses1.WalkSpeed   = 3                         #- Sprint Ability    
+        
+    pygame.display.flip()                    #- update the screen with the new drawing
+
+    #- Time delay
+    Clock.tick (Fps)                    #- delay the loop according to the set FPS (frames per second)
+    return Program
+
   
-  
+      
+  Program(Zombie1,Player2,SprintBarX)
+
   #Notes:
   #     That point of one zombie being just a circle that is cut off is intencional. DO NOT CORRECT MY PROGRAM
   #     Plans: Start screen and End screen , More Classes
